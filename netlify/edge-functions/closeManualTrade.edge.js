5213ac94623d2e0b61b7080c9a0ff1bb14551531
CRT-AUTO <crtautomations@gmail.com>
2025-05-10 21:48:16 +0700

    Update closeManualTrade.edge.js

--- netlify/edge-functions/closeManualTrade.edge.js
+++ netlify/edge-functions/closeManualTrade.edge.js
@@ -149,7 +149,6 @@ function calculateRMultiple(pnl, maxRisk) {
 
 export default async function handler(request, context) {
   console.log("Edge Function: closeManualTrade started");
-  console.log("Received request data:", requestData);
   
   // Handle preflight requests
   if (request.method === "OPTIONS") {
